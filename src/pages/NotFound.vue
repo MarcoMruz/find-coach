bdc3381b2e2156dd949a1efae72e59ba7a476e20
Marco Mrúz <marcomruz1@gmail.com>
2021-05-23 16:36:39 +0200

    add not found page handler

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
-  <div></div>
+  <VCard>
+    <h2>The page you are looking for was not found!</h2>
+    <p>
+      Maybe you will have more luck on this
+      <router-link to="/coaches">page</router-link>
+    </p>
+  </VCard>
 </template>
-<script>
-export default {};
-</script>
-<style scoped></style>
